de0881e8306d63b9274bb627f21e3d81ca51d030
jonathan <jonathan@kyuss.org>
2013-05-26 02:50:20 -0700

    Add more constants

--- src/main/java/org/cobbzilla/util/http/HttpStatusCodes.java
+++ src/main/java/org/cobbzilla/util/http/HttpStatusCodes.java
@@ -3,5 +3,11 @@ package org.cobbzilla.util.http;
 public class HttpStatusCodes {
 
     public static final int UNPROCESSABLE_ENTITY = 422;
+    public static final int OK = 200;
+    public static final int CREATED = 201;
+    public static final int NO_CONTENT = 204;
+    public static final int NOT_FOUND = 404;
+    public static final int FORBIDDEN = 403;
+    public static final int UNSUPPORTED_MEDIA_TYPE = 415;
 
 }
