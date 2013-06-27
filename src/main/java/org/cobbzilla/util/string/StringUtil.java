21c8a239c03dc0647a908ce0defd2e5118ece385
jonathan <jonathan@kyuss.org>
2013-06-27 09:03:20 -0700

    add UTF8 constant

--- src/main/java/org/cobbzilla/util/string/StringUtil.java
+++ src/main/java/org/cobbzilla/util/string/StringUtil.java
@@ -8,6 +8,7 @@ import java.util.Locale;
 
 public class StringUtil {
 
+    public static final String UTF8 = "UTF-8";
     public static final String EMPTY = "";
     public static final String DEFAULT_LOCALE = "en_US";
 
