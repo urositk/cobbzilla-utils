53024bf6d6673c641fcf1103090bdacd4636d746
jonathan <jonathan@kyuss.org>
2013-10-15 04:47:26 -0700

    use our own constants class, fewer dependencies

---
+++ src/main/java/org/cobbzilla/util/http/HttpMethods.java
@@ -0,0 +1,10 @@
+package org.cobbzilla.util.http;
+
+public class HttpMethods {
+
+    public static final String GET = "GET";
+    public static final String POST = "POST";
+    public static final String PUT = "PUT";
+    public static final String DELETE = "DELETE";
+
+}
