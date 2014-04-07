a8587d5461c00bcfca88054665f0ee00d6664dad
jonathan <jonathan@kyuss.org>
2014-04-07 14:44:36 -0700

    add formatDurationFrom utility

--- src/main/java/org/cobbzilla/util/time/TimeUtil.java
+++ src/main/java/org/cobbzilla/util/time/TimeUtil.java
@@ -4,7 +4,14 @@ import org.cobbzilla.util.string.StringUtil;
 import org.joda.time.DateTime;
 import org.joda.time.format.DateTimeFormatter;
 
+import java.util.concurrent.TimeUnit;
+
 public class TimeUtil {
+
+    public static final long DAY    = TimeUnit.DAYS.toMillis(1);
+    public static final long HOUR   = TimeUnit.HOURS.toMillis(1);
+    public static final long MINUTE = TimeUnit.MINUTES.toMillis(1);
+    public static final long SECOND = TimeUnit.SECONDS.toMillis(1);
 
     public static Long parse(String time, DateTimeFormatter formatter) {
         return StringUtil.empty(time) ? null : formatter.parseDateTime(time).getMillis();
@@ -12,5 +19,30 @@ public class TimeUtil {
 
     public static String format(Long time, DateTimeFormatter formatter) {
         return time == null ? null : new DateTime(time).toString(formatter);
+    }
+
+    public static String formatDurationFrom(long start) {
+
+        long duration = System.currentTimeMillis() - start;
+        long days = 0, hours = 0, mins = 0, secs = 0, millis = 0;
+
+        if (duration > DAY) {
+            days = duration/DAY;
+            duration -= days * DAY;
+        }
+        if (duration > HOUR) {
+            hours = duration/HOUR;
+            duration -= hours * HOUR;
+        }
+        if (duration > MINUTE) {
+            mins = duration/MINUTE;
+            duration -= mins * MINUTE;
+        }
+        if (duration > SECOND) {
+            secs = duration/SECOND;
+            millis = duration - secs * SECOND;
+        }
+
+        return String.format("%1$01dd %2$02d:%3$02d:%4$02d.%5$04d", days, hours, mins, secs, millis);
     }
 }
