83145ca754a379e2e59060dc390367318620d721
jonathan <jonathan@kyuss.org>
2014-04-07 03:40:48 -0700

    add host/port/path getters

--- src/main/java/org/cobbzilla/util/http/HttpRequestBean.java
+++ src/main/java/org/cobbzilla/util/http/HttpRequestBean.java
@@ -1,9 +1,11 @@
 package org.cobbzilla.util.http;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
+
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.util.Collections;
+import java.util.Map;
 
 @NoArgsConstructor @AllArgsConstructor
 public class HttpRequestBean<T> {
@@ -11,10 +13,31 @@ public class HttpRequestBean<T> {
     @Getter @Setter private String method;
     @Getter @Setter private String uri;
     @Getter @Setter private T data;
+    @Getter @Setter private Map<String, String> headers = Collections.EMPTY_MAP;
+
+    public HttpRequestBean (String method, String uri, T data) {
+        this(method, uri, data, Collections.EMPTY_MAP);
+    }
+
+    public boolean hasData () { return data != null; }
+
+    @Getter(lazy=true, value=AccessLevel.PRIVATE) private final URI _uri = initURI();
+
+    private URI initURI() {
+        try {
+            return new URI(uri);
+        } catch (URISyntaxException e) {
+            throw new IllegalStateException("initURI: "+e, e);
+        }
+    }
 
     public HttpRequestBean (String method, String uri) {
         this.method = method;
         this.uri = uri;
     }
+
+    public String getHost () { return get_uri().getHost(); }
+    public int getPort () { return get_uri().getPort(); }
+    public String getPath() { return get_uri().getPath(); }
 
 }
