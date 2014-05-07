c4e0247f94ae95f35cb93088acd36d3bc9f1c2f1
jonathan <jonathan@kyuss.org>
2014-05-06 18:07:18 -0700

    add support for .tar.bz2 files

--- src/main/java/org/cobbzilla/util/io/Tarball.java
+++ src/main/java/org/cobbzilla/util/io/Tarball.java
@@ -2,18 +2,22 @@ package org.cobbzilla.util.io;
 
 import com.google.common.io.Files;
 import lombok.Cleanup;
+import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.compress.archivers.ArchiveException;
 import org.apache.commons.compress.archivers.ArchiveStreamFactory;
 import org.apache.commons.compress.archivers.tar.TarArchiveEntry;
 import org.apache.commons.compress.archivers.tar.TarArchiveInputStream;
+import org.apache.commons.compress.compressors.CompressorInputStream;
+import org.apache.commons.compress.compressors.bzip2.BZip2CompressorInputStream;
 import org.apache.commons.compress.compressors.gzip.GzipCompressorInputStream;
 
 import java.io.*;
 
+@Slf4j
 public class Tarball {
 
     /**
-     * @param tarball the tarball to unroll
+     * @param tarball the tarball to unroll. Can be .tar.gz or .tar.bz2
      * @return a File representing the temp directory where the tarball was unrolled
      */
     public static File unroll (File tarball) throws IOException, ArchiveException {
@@ -23,9 +27,24 @@ public class Tarball {
 
     public static File unroll(File tarball, File dir) throws IOException, ArchiveException {
 
+        final String path = tarball.getAbsolutePath();
+        final FileInputStream fileIn = new FileInputStream(tarball);
+        final CompressorInputStream zipIn;
+
+        if (path.endsWith(".gz") || path.endsWith(".tgz")) {
+            zipIn = new GzipCompressorInputStream(fileIn);
+
+        } else if (path.endsWith(".bz2")) {
+            zipIn = new BZip2CompressorInputStream(fileIn);
+
+        } else {
+            log.warn("tarball (" + path + ") was not .tar.gz, .tgz, or .tar.bz2, assuming .tar.gz");
+            zipIn = new GzipCompressorInputStream(fileIn);
+        }
+
         @Cleanup final TarArchiveInputStream tarIn
                 = (TarArchiveInputStream) new ArchiveStreamFactory()
-                .createArchiveInputStream("tar", new GzipCompressorInputStream(new FileInputStream(tarball)));
+                .createArchiveInputStream("tar", zipIn);
 
         TarArchiveEntry entry;
         while ((entry = tarIn.getNextTarEntry()) != null) {
@@ -42,7 +61,7 @@ public class Tarball {
             }
             try (OutputStream out = new FileOutputStream(new File(dir, name))) {
                 if (StreamUtil.copyNbytes(tarIn, out, entry.getSize()) != entry.getSize()) {
-                    throw new IllegalStateException("Expected to copy "+entry.getSize()+ " bytes for "+entry.getName()+" in tarball "+tarball.getAbsolutePath());
+                    throw new IllegalStateException("Expected to copy "+entry.getSize()+ " bytes for "+entry.getName()+" in tarball "+ path);
                 }
             }
         }
