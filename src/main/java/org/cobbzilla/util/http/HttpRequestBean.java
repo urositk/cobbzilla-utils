bbaf584c1b06378aa19d4dca9beae72523df06db
jonathan <jonathan@kyuss.org>
2014-06-26 15:43:51 -0700

    use lombok chained accessors instead of wither methods

--- src/main/java/org/cobbzilla/util/http/HttpRequestBean.java
+++ src/main/java/org/cobbzilla/util/http/HttpRequestBean.java
@@ -3,25 +3,21 @@ package org.cobbzilla.util.http;
 import com.google.common.collect.ArrayListMultimap;
 import com.google.common.collect.Multimap;
 import lombok.*;
+import lombok.experimental.Accessors;
 import org.cobbzilla.util.string.StringUtil;
 
 import java.net.URI;
 
-@NoArgsConstructor @ToString(of={"method", "uri"})
+@NoArgsConstructor @ToString(of={"method", "uri"}) @Accessors(chain=true)
 public class HttpRequestBean<T> {
 
     @Getter @Setter private String method = HttpMethods.GET;
-    public HttpRequestBean<T> withMethod (String m) { method = m; return this; }
-
     @Getter @Setter private String uri;
-    public HttpRequestBean<T> withUri (String u) { uri = u; return this; }
 
     @Getter @Setter private T data;
-    public HttpRequestBean<T> withData (T d) { data = d; return this; }
     public boolean hasData () { return data != null; }
 
     @Getter @Setter private Multimap<String, String> headers = ArrayListMultimap.create();
-    public HttpRequestBean<T> withHeaders (Multimap<String, String> headers) { this.headers = headers; return this; }
     public HttpRequestBean<T> withHeader (String name, String value) { setHeader(name, value); return this; }
     public void setHeader (String name, String value) {
         headers.put(name, value);
