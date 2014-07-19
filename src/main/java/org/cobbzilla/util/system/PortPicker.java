484996d0a2e020a5f3be392005454f7e960fdc54
jonathan <jonathan@kyuss.org>
2014-07-18 22:00:31 -0700

    add pickOrDie

--- src/main/java/org/cobbzilla/util/system/PortPicker.java
+++ src/main/java/org/cobbzilla/util/system/PortPicker.java
@@ -4,9 +4,19 @@ import java.io.IOException;
 import java.net.ServerSocket;
 
 public class PortPicker {
-    public static int pick () throws IOException {
+
+    public static int pick() throws IOException {
         try (ServerSocket s = new ServerSocket(0)) {
             return s.getLocalPort();
         }
     }
+
+    public static int pickOrDie() {
+        try {
+            return pick();
+        } catch (IOException e) {
+            throw new IllegalStateException("Error picking port: "+e, e);
+        }
+    }
+
 }
