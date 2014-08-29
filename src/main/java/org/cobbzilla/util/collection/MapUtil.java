95d49bf389a44050e34a4e1c11bd366c940d1a83
jonathan <jonathan@kyuss.org>
2014-08-28 18:21:00 -0700

    add deepEquals and deepHash

--- src/main/java/org/cobbzilla/util/collection/MapUtil.java
+++ src/main/java/org/cobbzilla/util/collection/MapUtil.java
@@ -11,4 +11,27 @@ public class MapUtil {
         return map;
     }
 
+    public static <K, V> boolean deepEquals (Map<K, V>  m1, Map<K, V>  m2) {
+        if (m1 == null) return m2 == null;
+        if (m2 == null) return false;
+        if (m1.size() != m2.size()) return false;
+        final Set<Map.Entry<K, V>> set = m1.entrySet();
+        for (Map.Entry<K, V>  e : set) {
+            V m1v = e.getValue();
+            V m2v = m2.get(e.getKey());
+            if (m2v == null) return false;
+            if ((m1v instanceof Map && !deepEquals((Map<K,V>) m1v, (Map<K,V>) m2v)) || !m1v.equals(m2v)) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static <K, V> int deepHash(Map<K, V> m) {
+        int hash = 0;
+        for (Map.Entry<K, V>  e : m.entrySet()) {
+            hash = (31 * hash) + e.getKey().hashCode() + (31 * e.getValue().hashCode());
+        }
+        return hash;
+    }
 }
