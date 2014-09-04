9a070c67c8a9042ba3029b0c932b5aa15f2f0742
jonathan <jonathan@kyuss.org>
2014-09-04 13:37:47 -0700

    move ApiConnectionInfo here from cobbzilla-wizard

---
+++ src/main/java/org/cobbzilla/util/http/ApiConnectionInfo.java
@@ -0,0 +1,23 @@
+package org.cobbzilla.util.http;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@NoArgsConstructor @AllArgsConstructor
+public class ApiConnectionInfo {
+
+    @Getter @Setter private String baseUri;
+    @Getter @Setter private String user;
+    @Getter @Setter private String password;
+
+    public ApiConnectionInfo (String baseUri) {
+        this.baseUri = baseUri;
+    }
+
+    // alias for when this is used in json with snake_case naming conventions
+    public String getBase_uri () { return getBaseUri(); }
+    public void setBase_uri (String uri) { setBaseUri(uri); }
+
+}
