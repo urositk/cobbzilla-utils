a3927bdcec146f405b0932b3235036f8aa744669
jonathan <jonathan@kyuss.org>
2014-09-06 20:47:51 -0700

    allow methods to throw Exception

--- src/main/java/org/cobbzilla/util/dns/DnsManager.java
+++ src/main/java/org/cobbzilla/util/dns/DnsManager.java
@@ -2,11 +2,11 @@ package org.cobbzilla.util.dns;
 
 public interface DnsManager {
 
-    public void writeA(String hostname, String ip, int ttl);
+    public void writeA(String hostname, String ip, int ttl) throws Exception;
 
-    public void writeCNAME(String hostname, String name, int ttl);
+    public void writeCNAME(String hostname, String name, int ttl) throws Exception;
 
-    public void writeMX(String mailDomain, String mxHostname, int rank, int ttl);
+    public void writeMX(String mailDomain, String mxHostname, int rank, int ttl) throws Exception;
 
     public void removeAll(String domain);
 
