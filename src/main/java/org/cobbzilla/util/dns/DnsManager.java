9a157af87c868c38bc72fc7f41390d52c02e6b8e
jonathan <jonathan@kyuss.org>
2014-09-08 11:42:27 -0700

    rename param

--- src/main/java/org/cobbzilla/util/dns/DnsManager.java
+++ src/main/java/org/cobbzilla/util/dns/DnsManager.java
@@ -2,11 +2,11 @@ package org.cobbzilla.util.dns;
 
 public interface DnsManager {
 
-    public void writeA(String hostname, String ip, int ttl) throws Exception;
+    public void writeA(String fqdn, String ip, int ttl) throws Exception;
 
-    public void writeCNAME(String hostname, String name, int ttl) throws Exception;
+    public void writeCNAME(String fqdn, String name, int ttl) throws Exception;
 
-    public void writeMX(String mailDomain, String mxHostname, int rank, int ttl) throws Exception;
+    public void writeMX(String fqdn, String mxHostname, int rank, int ttl) throws Exception;
 
     public void removeAll(String domain);
 
