00a43c1209f50ac7b33655bc8cf84d5e90e5d61c
jonathan <jonathan@kyuss.org>
2014-09-15 23:54:42 -0700

    add utils to modify json documents

--- src/main/java/org/cobbzilla/util/json/JsonUtil.java
+++ src/main/java/org/cobbzilla/util/json/JsonUtil.java
@@ -1,15 +1,25 @@
 package org.cobbzilla.util.json;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
-import com.fasterxml.jackson.databind.MapperFeature;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.ObjectWriter;
-import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.databind.*;
+import com.fasterxml.jackson.databind.node.*;
+import org.cobbzilla.util.io.FileSuffixFilter;
+import org.cobbzilla.util.io.FilenameSuffixFilter;
 import org.cobbzilla.util.string.StringUtil;
 
+import java.io.File;
+import java.io.FileFilter;
+import java.io.FilenameFilter;
 import java.io.IOException;
+import java.math.BigDecimal;
+import java.math.BigInteger;
+import java.util.ArrayList;
+import java.util.List;
 
 public class JsonUtil {
+
+    public static final FileFilter JSON_FILES = new FileSuffixFilter(".json");
+    public static final FilenameFilter JSON_FILENAMES = new FilenameSuffixFilter(".json");
 
     public static final ObjectMapper FULL_MAPPER = new ObjectMapper()
             .configure(SerializationFeature.INDENT_OUTPUT, true);
@@ -56,6 +66,80 @@ public class JsonUtil {
         } catch (IOException e) {
             throw new IllegalStateException("fromJson: exception while reading: "+json+": "+e, e);
         }
+    }
+
+    public static <T> T fromJson(String json, String path, Class<T> clazz) throws Exception {
+        JsonNode node = findNode(FULL_MAPPER.readTree(json), path);
+        return FULL_MAPPER.convertValue(node, clazz);
+    }
+
+    public static JsonNode findNode(JsonNode node, String path) throws IOException {
+        final List<JsonNode> nodePath = findNodePath(node, path);
+        return nodePath == null || nodePath.isEmpty() ? null : nodePath.get(nodePath.size()-1);
+    }
+
+    public static List<JsonNode> findNodePath(JsonNode node, String path) throws IOException {
+        final List<JsonNode> nodePath = new ArrayList<>();
+        final String[] pathParts = path.split("\\.");
+        for (String pathPart : pathParts) {
+            int index = -1;
+            int bracketPos = pathPart.indexOf("[");
+            int bracketClosePos = pathPart.indexOf("]");
+            if (bracketPos != -1 && bracketClosePos != -1 && bracketClosePos > bracketPos) {
+                index = Integer.parseInt(pathPart.substring(bracketPos+1, bracketClosePos));
+                pathPart = pathPart.substring(0, bracketPos);
+            }
+            node = node.get(pathPart);
+            nodePath.add(node);
+            if (index != -1) {
+                node = node.get(index);
+                nodePath.add(node);
+            }
+        }
+        return nodePath;
+    }
+
+    public static ObjectNode replaceNode(File file, String path, String replacement) throws Exception {
+        return replaceNode((ObjectNode) FULL_MAPPER.readTree(file), path, replacement);
+    }
+
+    public static ObjectNode replaceNode(String json, String path, String replacement) throws Exception {
+        return replaceNode((ObjectNode) FULL_MAPPER.readTree(json), path, replacement);
+    }
+
+    public static ObjectNode replaceNode(ObjectNode document, String path, String replacement) throws Exception {
+
+        final String simplePath = path.contains(".") ? path.substring(path.lastIndexOf(".")+1) : path;
+        Integer index = null;
+        if (simplePath.contains("[")) {
+            index = Integer.parseInt(simplePath.substring(simplePath.indexOf("[")+1, simplePath.indexOf("]")));
+        }
+        final List<JsonNode> found = findNodePath(document, path);
+        if (found == null || found.isEmpty()) throw new IllegalArgumentException("path not found: "+path);
+
+        final JsonNode parent = found.size() > 1 ? found.get(found.size()-2) : document;
+        if (index != null) {
+            final JsonNode origNode = ((ArrayNode) parent).get(index);
+            ((ArrayNode) parent).set(index, getValueNode(origNode, path, replacement));
+        } else {
+            // what is the original node type?
+            final JsonNode origNode = parent.get(simplePath);
+            ((ObjectNode) parent).put(simplePath, getValueNode(origNode, path, replacement));
+        }
+        return document;
+    }
+
+    public static JsonNode getValueNode(JsonNode node, String path, String replacement) {
+        final String nodeClass = node.getClass().getName();
+        if ( ! (node instanceof ValueNode) ) throw new IllegalStateException("Path "+path+" does not refer to a value (it is a "+ nodeClass +")");
+        if (node instanceof TextNode) return new TextNode(replacement);
+        if (node instanceof BooleanNode) return BooleanNode.valueOf(Boolean.parseBoolean(replacement));
+        if (node instanceof IntNode) return new IntNode(Integer.parseInt(replacement));
+        if (node instanceof LongNode) return new LongNode(Long.parseLong(replacement));
+        if (node instanceof DoubleNode) return new DoubleNode(Double.parseDouble(replacement));
+        if (node instanceof DecimalNode) return new DecimalNode(new BigDecimal(replacement));
+        if (node instanceof BigIntegerNode) return new BigIntegerNode(new BigInteger(replacement));
+        throw new IllegalArgumentException("Path "+path+" refers to an unsupported ValueNode: "+ nodeClass);
     }
 
 }
---
+++ src/test/java/org/cobbzilla/util/json/JsonUtilTest.java
@@ -0,0 +1,73 @@
+package org.cobbzilla.util.json;
+
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.node.ObjectNode;
+import org.apache.commons.lang.RandomStringUtils;
+import org.cobbzilla.util.io.FileUtil;
+import org.cobbzilla.util.io.StreamUtil;
+import org.cobbzilla.util.string.StringUtil;
+import org.junit.Test;
+
+import java.io.File;
+
+import static org.junit.Assert.assertEquals;
+
+public class JsonUtilTest {
+
+    private static final String TEST_JSON = StringUtil.getPackagePath(JsonUtilTest.class)+"/test.json";
+    private static final Object[][] TESTS = new Object[][] {
+        new Object[] {
+                "id", new ReplacementValue() { @Override public String getValue(TestData testData) { return testData.id; } }
+        },
+        new Object[] {
+                "thing.field1[1]", new ReplacementValue() { @Override public String getValue(TestData testData) { return testData.thing.field1[1]; } }
+        },
+        new Object[] {
+                "another_thing.field1", new ReplacementValue() { @Override public String getValue(TestData testData) { return testData.another_thing.field1; } }
+        },
+    };
+
+    @Test
+    public void testReplaceJsonValue () throws Exception {
+
+        final String testJson = StreamUtil.loadResourceAsString(TEST_JSON);
+        final String replacement = RandomStringUtils.randomAlphanumeric(10);
+
+        for (Object[] test : TESTS) {
+            assertReplacementMade(testJson, replacement, (String) test[0], (ReplacementValue) test[1]);
+        }
+    }
+
+    public void assertReplacementMade(String testJson, String replacement, String path, ReplacementValue value) throws Exception {
+        final ObjectNode doc = JsonUtil.replaceNode(testJson, path, replacement);
+        final File temp = File.createTempFile("JsonUtilTest", ".json");
+        FileUtil.toFile(temp, JsonUtil.toJson(doc));
+        final TestData data = JsonUtil.fromJson(FileUtil.toString(temp), TestData.class);
+        assertEquals(replacement, value.getValue(data));
+    }
+
+    public static interface ReplacementValue {
+        public String getValue(TestData testData);
+    }
+
+    public static class TestData {
+        public String id;
+        public Thing thing;
+        public AnotherThing another_thing;
+    }
+
+    public static class Thing {
+        public String[] field1;
+        public Field2 field2;
+    }
+
+    public static class Field2 {
+        public String subfieldA;
+        public String subB;
+    }
+
+    public static class AnotherThing {
+        public String field1;
+        public JsonNode fieldZ;
+    }
+}
