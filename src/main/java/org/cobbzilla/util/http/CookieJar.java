c2b220973801289c0d1efbae07ba66c3455bb693
jonathan <jonathan@kyuss.org>
2014-10-13 19:02:31 -0700

    add constructor that takes a list of cookies

--- src/main/java/org/cobbzilla/util/http/CookieJar.java
+++ src/main/java/org/cobbzilla/util/http/CookieJar.java
@@ -1,12 +1,18 @@
 package org.cobbzilla.util.http;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
+import lombok.NoArgsConstructor;
 import org.cobbzilla.util.collection.CaseInsensitiveStringKeyMap;
 
 import java.util.ArrayList;
 import java.util.List;
 
+@NoArgsConstructor
 public class CookieJar extends CaseInsensitiveStringKeyMap<HttpCookieBean> {
+
+    public CookieJar(List<HttpCookieBean> cookies) {
+        for (HttpCookieBean cookie : cookies) add(cookie);
+    }
 
     public void add (HttpCookieBean cookie) {
         if (cookie.expired()) {
