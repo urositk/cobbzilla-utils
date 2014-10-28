87560c96b375d5ea797fcaffd2bdd1274afba0a2
jonathan <jonathan@kyuss.org>
2014-10-28 09:24:56 -0700

    pull in entity as byte array, before stream is closed

--- src/main/java/org/cobbzilla/util/http/HttpResponseBean.java
+++ src/main/java/org/cobbzilla/util/http/HttpResponseBean.java
@@ -5,7 +5,10 @@ import com.google.common.collect.Multimap;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.io.IOUtils;
+import org.cobbzilla.util.json.JsonUtil;
 
+import java.io.IOException;
 import java.io.InputStream;
 
 @Slf4j
@@ -13,7 +16,7 @@ public class HttpResponseBean {
 
     @Getter @Setter private int status;
     @Getter @Setter private Multimap<String, String> headers;
-    @Getter @Setter private InputStream entity;
+    @Getter @Setter private byte[] entity;
     @Getter @Setter private long contentLength;
     @Getter @Setter private String contentType;
 
@@ -22,13 +25,20 @@ public class HttpResponseBean {
         headers.put(name, value);
     }
 
-    public void close () {
-        if (entity != null) {
-            try {
-                entity.close();
-            } catch (Exception e) {
-                log.warn("error closing stream: "+e, e);
-            }
+    public void setEntity (InputStream entity) {
+        try {
+            this.entity = entity == null ? null : IOUtils.toByteArray(entity);
+        } catch (IOException e) {
+            throw new IllegalStateException("setEntity: error reading stream: "+e, e);
         }
     }
+
+    public String getEntityString () {
+        return entity == null ? null : new String(entity);
+    }
+
+    public <T> T getEntity (Class<T> clazz) {
+        return entity == null ? null : JsonUtil.fromJsonOrDie(getEntityString(), clazz);
+    }
+
 }
