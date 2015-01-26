f207ce1844041755b8b5aa3c26d74c72465927f1
jonathan <jonathan@kyuss.org>
2015-01-25 23:27:48 -0800

    add robustness

--- src/main/java/org/cobbzilla/util/io/FilesystemWatcher.java
+++ src/main/java/org/cobbzilla/util/io/FilesystemWatcher.java
@@ -2,6 +2,7 @@ package org.cobbzilla.util.io;
 
 import lombok.Getter;
 import lombok.extern.slf4j.Slf4j;
+import org.cobbzilla.util.system.Sleep;
 
 import java.io.File;
 import java.nio.file.*;
@@ -82,6 +83,18 @@ public class FilesystemWatcher implements Runnable {
 
     public File toFile(Path p) { return new File(path.toFile(), p.toFile().getName()); }
 
+    /**
+     * If the path does not exist, we cannot create the watch. But we can keep trying, and we do.
+     * @return how long to wait before retrying to create the watch, if the path didn't exist
+     */
+    protected long getSleepWhileNotExists() { return 10_000; }
+
+    /**
+     * If null is returned, the watcher will terminate on any unexpected Exception
+     * @return how long to sleep after some other unknown Exception (besides InterruptedException) occurs.
+     */
+    protected Integer getSleepAfterUnexpectedError() { return 10_000; }
+
     @Override
     public void run() {
         try {
@@ -114,10 +127,21 @@ public class FilesystemWatcher implements Runnable {
             }
 
         } catch (InterruptedException e) {
-            throw new IllegalStateException("watch thread interrupted, exiting: "+e, e);
+            throw new IllegalStateException("watch thread interrupted, exiting: " + e, e);
+
+        } catch (NoSuchFileException e) {
+            log.warn("watch dir does not exist, waiting for it to exist: "+ e);
+            Sleep.sleep(getSleepWhileNotExists(), "waiting for path to exist: "+path.toFile().getAbsolutePath());
 
         } catch (Exception e) {
-            throw new IllegalStateException("error in watch thread, exiting: "+e, e);
+            if (getSleepAfterUnexpectedError() != null) {
+                log.warn("error in watch thread, waiting to re-create the watch: "+e, e);
+                Sleep.sleep(getSleepAfterUnexpectedError());
+
+            } else {
+                throw new IllegalStateException("error in watch thread, exiting: " + e, e);
+            }
         }
     }
+
 }
