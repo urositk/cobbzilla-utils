6dcc53fffbafa1f42e210f6e9017718e9fe9016c
jonathan <jonathan@kyuss.org>
2015-03-09 19:11:06 -0700

    more sleep

--- src/main/java/org/cobbzilla/util/system/Sleep.java
+++ src/main/java/org/cobbzilla/util/system/Sleep.java
@@ -4,17 +4,13 @@ import static org.cobbzilla.util.daemon.ZillaRuntime.die;
 
 public class Sleep {
 
-    public static void sleep (long millis) {
-        sleep(millis, "no reason for sleep given", null);
-    }
+    public static void sleep () { sleep(100); }
 
-    public static void sleep (long millis, String reason) {
-        sleep(millis, reason, null);
-    }
+    public static void sleep (long millis) { sleep(millis, "no reason for sleep given", null); }
 
-    public static void sleep (long millis, Exception cause) {
-        sleep(millis, null, cause);
-    }
+    public static void sleep (long millis, String reason) { sleep(millis, reason, null); }
+
+    public static void sleep (long millis, Exception cause) { sleep(millis, null, cause); }
 
     public static void sleep (long millis, String reason, Exception cause) {
         try {
