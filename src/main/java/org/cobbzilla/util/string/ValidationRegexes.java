2086d8ebd02b8fc2d23848438f39774ef70fbb44
jonathan <jonathan@kyuss.org>
2015-08-04 16:32:36 -0700

    move ValidationRegexes to cobbzilla-utils

---
+++ src/main/java/org/cobbzilla/util/string/ValidationRegexes.java
@@ -0,0 +1,32 @@
+package org.cobbzilla.util.string;
+
+import java.util.regex.Pattern;
+
+public class ValidationRegexes {
+
+    public static final Pattern LOGIN_PATTERN = pattern("^[\\w\\-]+$");
+    public static final Pattern EMAIL_PATTERN = pattern("^[A-Z0-9][A-Z0-9._%+-]*@[A-Z0-9.-]+\\.[A-Z]{2,6}$");
+    public static final Pattern EMAIL_NAME_PATTERN = pattern("^[A-Z0-9][A-Z0-9._%+-]*$");
+
+    public static final Pattern[] LOCALE_PATTERNS = {
+            pattern("^[a-zA-Z]{2,3}([-_][a-zA-z]{2}(@[\\w]+)?)?"), // ubuntu style: en_US or just en
+            pattern("^[a-zA-Z]{2,3}([-_][\\w]+)?"),             // some apps use style: ca-valencia
+    };
+
+    public static final Pattern IPv4_PATTERN = pattern("^(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)(\\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)){3}$");
+    public static final Pattern IPv6_PATTERN = pattern("^(([0-9a-fA-F]{1,4}:){7,7}[0-9a-fA-F]{1,4}|([0-9a-fA-F]{1,4}:){1,7}:|([0-9a-fA-F]{1,4}:){1,6}:[0-9a-fA-F]{1,4}|([0-9a-fA-F]{1,4}:){1,5}(:[0-9a-fA-F]{1,4}){1,2}|([0-9a-fA-F]{1,4}:){1,4}(:[0-9a-fA-F]{1,4}){1,3}|([0-9a-fA-F]{1,4}:){1,3}(:[0-9a-fA-F]{1,4}){1,4}|([0-9a-fA-F]{1,4}:){1,2}(:[0-9a-fA-F]{1,4}){1,5}|[0-9a-fA-F]{1,4}:((:[0-9a-fA-F]{1,4}){1,6})|:((:[0-9a-fA-F]{1,4}){1,7}|:)|fe80:(:[0-9a-fA-F]{0,4}){0,4}%[0-9a-zA-Z]{1,}|::(ffff(:0{1,4}){0,1}:){0,1}((25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9])\\.){3,3}(25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9])|([0-9a-fA-F]{1,4}:){1,4}:((25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9])\\.){3,3}(25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9]))$");
+
+    public static final Pattern HOST_PATTERN  = pattern("^([A-Z0-9]|[A-Z0-9][A-Z0-9\\-]{0,61}[A-Z0-9])(\\.([A-Z0-9]|[A-Z0-9][A-Z0-9\\-]{0,61}[A-Z0-9]))*$");
+    public static final Pattern HOST_PART_PATTERN  = pattern("^([A-Z0-9]|[A-Z0-9][A-Z0-9\\-]{0,61}[A-Z0-9])$");
+    public static final Pattern PORT_PATTERN  = pattern("^[\\d]{1,5}$");
+
+    public static final Pattern URL_PATTERN   = pattern("^(https?|ftp|file)://[-a-zA-Z0-9+&@#/%?=~_|!:,.;]*[-a-zA-Z0-9+&@#/%=~_|]$");
+    public static final Pattern HTTP_PATTERN  = pattern("^(https?)://[-a-zA-Z0-9+&@#/%?=~_|!:,.;]*[-a-zA-Z0-9+&@#/%=~_|]$");
+    public static final Pattern HTTPS_PATTERN = pattern("^https://[-a-zA-Z0-9+&@#/%?=~_|!:,.;]*[-a-zA-Z0-9+&@#/%=~_|]$");
+
+    public static final Pattern FILENAME_PATTERN = pattern("^[_A-Z0-9\\-\\.]+$");
+    public static final Pattern INTEGER_PATTERN = pattern("^[0-9]+$");
+
+    private static Pattern pattern(String regex) { return Pattern.compile(regex, Pattern.CASE_INSENSITIVE); }
+
+}
--- src/main/java/org/cobbzilla/util/system/NetworkUtil.java
+++ src/main/java/org/cobbzilla/util/system/NetworkUtil.java
@@ -5,6 +5,8 @@ import java.net.NetworkInterface;
 import java.util.Enumeration;
 
 import static org.cobbzilla.util.daemon.ZillaRuntime.die;
+import static org.cobbzilla.util.daemon.ZillaRuntime.empty;
+import static org.cobbzilla.util.string.ValidationRegexes.IPv4_PATTERN;
 
 public class NetworkUtil {
 
@@ -25,6 +27,30 @@ public class NetworkUtil {
 
         } catch (Exception e) {
             return die("getLocalhostIpv4: "+e, e);
+        }
+    }
+
+    public static String getFirstPublicIpv4() {
+        try {
+            final Enumeration<NetworkInterface> interfaces = NetworkInterface.getNetworkInterfaces();
+            while (interfaces.hasMoreElements()) {
+                final NetworkInterface i = interfaces.nextElement();
+                if (!i.getName().startsWith("lo")) {
+                    final Enumeration<InetAddress> addrs = i.getInetAddresses();
+                    while (addrs.hasMoreElements()) {
+                        final String addr = addrs.nextElement().toString();
+                        if (!empty(addr) &&
+                                !addr.startsWith("/127") &&
+                                IPv4_PATTERN.matcher(addr.substring(1)).matches()) {
+                            return addr.substring(1);
+                        }
+                    }
+                }
+            }
+            return die("getFirstPublicIpv4: no non-local IPv4 address found");
+
+        } catch (Exception e) {
+            return die("getFirstPublicIpv4: "+e, e);
         }
     }
 }
