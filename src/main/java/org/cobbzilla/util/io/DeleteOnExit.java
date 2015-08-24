5bec471f32e851f08c30303e80f7661767a78986
jonathan <jonathan@kyuss.org>
2015-08-23 22:28:18 -0700

    add CloseOnExit. refactor DeleteOnExit

--- src/main/java/org/cobbzilla/util/io/DeleteOnExit.java
+++ src/main/java/org/cobbzilla/util/io/DeleteOnExit.java
@@ -1,35 +1,41 @@
 package org.cobbzilla.util.io;
 
-import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.io.FileUtils;
-import org.slf4j.Logger;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
-@AllArgsConstructor
+@Slf4j
 public class DeleteOnExit implements Runnable {
 
-    private Logger log;
-    private File path;
+    private static List<File> paths = new ArrayList<>();
 
-    @Override
-    public void run() {
-        if (!path.exists()) return;
-        if (path.isDirectory()) {
-            try {
-                FileUtils.deleteDirectory(path);
-            } catch (IOException e) {
-                log.warn("FileUtil.deleteOnExit: error deleting path="+path+": "+e, e);
-            }
-        } else {
-            if (!path.delete()) {
-                log.warn("FileUtil.deleteOnExit: error deleting path="+path);
+    private DeleteOnExit () {}
+
+    static {
+        Runtime.getRuntime().addShutdownHook(new Thread(new DeleteOnExit()));
+    }
+
+    @Override public void run() {
+        for (File path : paths) {
+            if (!path.exists()) return;
+            if (path.isDirectory()) {
+                try {
+                    FileUtils.deleteDirectory(path);
+                } catch (IOException e) {
+                    log.warn("FileUtil.deleteOnExit: error deleting path=" + path + ": " + e, e);
+                }
+            } else {
+                if (!path.delete()) {
+                    log.warn("FileUtil.deleteOnExit: error deleting path=" + path);
+                }
             }
         }
     }
 
-    public static void schedule(Logger log, File path) {
-        Runtime.getRuntime().addShutdownHook(new Thread(new DeleteOnExit(log, path)));
-    }
+    public static void add(File path) { paths.add(path); }
+
 }
