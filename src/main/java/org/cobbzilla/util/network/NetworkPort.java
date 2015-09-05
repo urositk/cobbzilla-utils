b7f90c36db8a0cd4ff046bdf8e1efb0b5711fbfe
jonathan <jonathan@kyuss.org>
2015-09-05 08:17:41 -0700

    simplify network utils

--- src/main/java/org/cobbzilla/util/network/NetworkPort.java
+++ src/main/java/org/cobbzilla/util/network/NetworkPort.java
@@ -5,12 +5,10 @@ import lombok.Setter;
 import lombok.experimental.Accessors;
 
 @Accessors(chain=true)
-public class FirewallRule {
+public class NetworkPort {
 
     @Getter @Setter Integer port;
     @Getter @Setter TransportProtocol protocol = TransportProtocol.tcp;
     @Getter @Setter NetworkInterfaceType iface = NetworkInterfaceType.world;
-    @Getter @Setter FirewallChain chain = FirewallChain.INPUT;
-    @Getter @Setter FirewallAction action = FirewallAction.ACCEPT;
 
 }
