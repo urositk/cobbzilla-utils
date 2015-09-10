8af12b06f7bed16da13663f76a80719f8413b99f
jonathan <jonathan@kyuss.org>
2015-09-10 09:21:26 -0700

    DnsRecordBase: fix match on value field

--- src/main/java/org/cobbzilla/util/dns/DnsRecordBase.java
+++ src/main/java/org/cobbzilla/util/dns/DnsRecordBase.java
@@ -3,8 +3,8 @@ package org.cobbzilla.util.dns;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.*;
 import lombok.experimental.Accessors;
-import org.apache.commons.lang3.StringUtils;
 
+import static org.apache.commons.lang3.StringUtils.chop;
 import static org.cobbzilla.util.daemon.ZillaRuntime.empty;
 
 @Accessors(chain=true) @ToString @NoArgsConstructor @AllArgsConstructor @EqualsAndHashCode
@@ -12,7 +12,7 @@ public class DnsRecordBase {
 
     @Getter @Setter private String fqdn;
     public boolean hasFqdn() { return !empty(fqdn); }
-    @JsonIgnore public String getNormalFqdn() { return empty(fqdn) ? fqdn : fqdn.endsWith(".") ? StringUtils.chop(fqdn) : fqdn; }
+    @JsonIgnore public String getNormalFqdn() { return empty(fqdn) ? fqdn : fqdn.endsWith(".") ? chop(fqdn) : fqdn; }
 
     @Getter @Setter private DnsType type;
     public boolean hasType () { return type != null; }
@@ -26,10 +26,13 @@ public class DnsRecordBase {
     }
 
     public boolean match(DnsRecordMatch match) {
+        // strip trailing dot if there is one
+        final String val = !hasValue() || !value.endsWith(".") ? value : chop(value);
+
         if (match.hasSubdomain() && !getNormalFqdn().endsWith(match.getSubdomain())) return false;
         if (match.hasType() && getType() != match.getType()) return false;
         if (match.hasFqdn() && !getNormalFqdn().equalsIgnoreCase(match.getFqdn())) return false;
-        if (match.hasValue() && !getValue().equalsIgnoreCase(match.getValue())) return false;
+        if (match.hasValue() && hasValue() && !val.equalsIgnoreCase(match.getValue())) return false;
         return true;
     }
 }
