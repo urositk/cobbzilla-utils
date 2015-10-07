8f1bf68823ade6eea3995a02fca6354c0645c08e
jonathan <jonathan@kyuss.org>
2015-10-07 16:53:07 -0700

    add poisonProxy utility

--- src/main/java/org/cobbzilla/util/reflect/ReflectionUtil.java
+++ src/main/java/org/cobbzilla/util/reflect/ReflectionUtil.java
@@ -12,6 +12,7 @@ import java.util.Map;
 
 import static com.google.common.base.Preconditions.checkNotNull;
 import static org.cobbzilla.util.daemon.ZillaRuntime.die;
+import static org.cobbzilla.util.daemon.ZillaRuntime.notSupported;
 import static org.cobbzilla.util.string.StringUtil.uncapitalize;
 
 /**
@@ -482,4 +483,20 @@ public class ReflectionUtil {
         return null;
     }
 
+    /**
+     * Create a proxy object for a class where calling any methods on the object will result in it thowing an exception.
+     * @param clazz The class to create a proxy for
+     * @param <T> The class to create a proxy for
+     * @return A proxy to the class that will throw an exception if any methods are called on it
+     */
+    public static <T> T poisonProxy(Class<T> clazz) {
+        return (T) Proxy.newProxyInstance(clazz.getClassLoader(), new Class[]{clazz}, PoisonedInvocationHandler.instance);
+    }
+
+    private static class PoisonedInvocationHandler implements InvocationHandler {
+        public static PoisonedInvocationHandler instance = new PoisonedInvocationHandler();
+        @Override public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
+            return notSupported("method not supported by poisonProxy: " + method.getName() + " (in fact, NO methods will work on this object)");
+        }
+    }
 }
