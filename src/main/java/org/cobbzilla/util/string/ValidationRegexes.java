a4d294e8ab0903da7eb46e9df25c6bd1a48c193c
jonathan <jonathan@kyuss.org>
2015-10-12 21:53:48 -0700

    rename DOB regex

--- src/main/java/org/cobbzilla/util/string/ValidationRegexes.java
+++ src/main/java/org/cobbzilla/util/string/ValidationRegexes.java
@@ -32,8 +32,8 @@ public class ValidationRegexes {
     public static final Pattern INTEGER_PATTERN = pattern("^[0-9]+$");
 
     // note: change this regex in the year 2099 or it will fail for people born in 2100
-    public static final String DOB_REGEX = "^(19|20)[0-9]{2}-[01][0-9]-(0[1-9]|[1-2][0-9]|3[0-1])$";
-    public static final Pattern DOB_PATTERN = pattern(DOB_REGEX);
+    public static final String YYYYMMDD_REGEX = "^(19|20)[0-9]{2}-[01][0-9]-(0[1-9]|[1-2][0-9]|3[0-1])$";
+    public static final Pattern YYYYMMDD_PATTERN = pattern(YYYYMMDD_REGEX);
 
     private static Pattern pattern(String regex) { return Pattern.compile(regex, Pattern.CASE_INSENSITIVE); }
 
