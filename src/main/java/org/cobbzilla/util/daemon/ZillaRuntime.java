7d7a2cfcf7b67d9d32a2c72818bc1691092b0293
jonathan <jonathan@kyuss.org>
2016-05-27 17:20:04 -0700

    adjust pct by 100 factor

--- src/main/java/org/cobbzilla/util/daemon/ZillaRuntime.java
+++ src/main/java/org/cobbzilla/util/daemon/ZillaRuntime.java
@@ -98,7 +98,9 @@ public class ZillaRuntime {
         return big(value).multiply(big(pct)).setScale(0, rounding).intValue();
     }
 
-    public static int percent(BigDecimal value, BigDecimal pct) { return percent(value.intValue(), pct.doubleValue(), RoundingMode.HALF_EVEN); }
+    public static int percent(BigDecimal value, BigDecimal pct) {
+        return percent(value.intValue(), pct.multiply(big(0.01)).doubleValue(), RoundingMode.HALF_EVEN);
+    }
 
     public static String uuid() { return UUID.randomUUID().toString(); }
 
