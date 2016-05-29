f35b162ee1cb82abd553c9e9ee8943eb8504a958
jonathan <jonathan@kyuss.org>
2016-05-29 03:13:12 -0700

    fix span merger

--- src/main/java/org/cobbzilla/util/xml/TidyHandlebarsSpanMerger.java
+++ src/main/java/org/cobbzilla/util/xml/TidyHandlebarsSpanMerger.java
@@ -1,48 +1,102 @@
 package org.cobbzilla.util.xml;
 
+import org.cobbzilla.util.collection.mappy.MappyList;
 import org.w3c.dom.Document;
+import org.w3c.dom.NamedNodeMap;
 import org.w3c.dom.Node;
 import org.w3c.dom.NodeList;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.Map;
 
 public class TidyHandlebarsSpanMerger implements TidyHelper {
 
     public static final TidyHandlebarsSpanMerger instance = new TidyHandlebarsSpanMerger();
 
-    @Override public void process(Document doc) { mergeSpans(doc); }
+    @Override public void process(Document doc) {
+        final MappyList<Node, Node> toRemove = new MappyList<>();
+        mergeSpans(doc, toRemove);
+        for (Map.Entry<Node, Node> n : toRemove.entrySet()) {
+            n.getKey().removeChild(n.getValue());
+        }
+    }
 
-    protected void mergeSpans(Node parent) {
-        Node firstSpan = null;
+    protected void mergeSpans(Node parent, MappyList<Node, Node> toRemove) {
+
+        Node spanStart = null;
+        StringBuilder spanTemp = null;
         NodeList childNodes = parent.getChildNodes();
-        List<Node> toRemove = new ArrayList<Node>();
-        for (int i = 0; i < childNodes.getLength(); i++) {
+        for (int i=0; i <childNodes.getLength(); i++) {
             final Node child = childNodes.item(i);
-            if (child.getNodeType() == Node.ELEMENT_NODE && child.getNodeName().equalsIgnoreCase("span") && child.hasChildNodes()) {
-                for (int j = 0; j < child.getChildNodes().getLength(); j++) {
-                    final Node subChild = child.getChildNodes().item(j);
-                    if (subChild.getNodeType() == Node.TEXT_NODE) {
-                        final String textContent = subChild.getNodeValue();
-                        if (firstSpan == null && textContent.startsWith("{{")) {
-                            firstSpan = subChild;
-                        } else if (firstSpan != null) {
-                            firstSpan.setNodeValue(firstSpan.getNodeValue() + textContent);
-                            toRemove.add(child);
-                            if (textContent.endsWith("}}")) {
-                                firstSpan = null;
-                            }
-                        }
+            if (child.getNodeType() == Node.ELEMENT_NODE) {
+                if (child.getNodeName().equalsIgnoreCase("span")) {
+                    if (spanStart == null) {
+                        spanStart = child;
+                        spanTemp = new StringBuilder(collectText(child));
+
+                    } else if (sameAttrs(spanStart.getAttributes(), child.getAttributes())) {
+                        //noinspection ConstantConditions
+                        append(spanTemp, collectText(child));
+                        spanStart.getFirstChild().setNodeValue(spanTemp.toString());
+                        toRemove.put(parent, child);
+                    } else {
+                        spanStart = child;
+                        spanTemp = new StringBuilder(collectText(child));
                     }
+                } else if (child.hasChildNodes()) {
+                    mergeSpans(child, toRemove);
+                }
+                continue;
+
+            } else if (child.getNodeType() == Node.TEXT_NODE) {
+                if (spanTemp != null) {
+                    append(spanTemp, child.getNodeValue());
+                    spanStart.getFirstChild().setNodeValue(spanTemp.toString());
+                    toRemove.put(parent, child);
+                    continue;
+                }
+            }
+            if (child.hasChildNodes()) {
+                mergeSpans(child, toRemove);
+            }
+        }
+        if (spanStart != null && spanTemp != null && spanTemp.length() > 0) {
+            spanStart.getFirstChild().setNodeValue(spanTemp.toString());
+        }
+    }
+
+    private StringBuilder append(StringBuilder b, String s) {
+        if (s == null || s.length() == 0) return b;
+        return b.append(s);
+    }
+
+    private String collectText(Node node) {
+        final StringBuilder b = new StringBuilder();
+        if (node.hasChildNodes()) {
+            final NodeList childNodes = node.getChildNodes();
+            for (int i=0; i<childNodes.getLength(); i++) {
+                final Node child = childNodes.item(i);
+                if (child.getNodeType() == Node.TEXT_NODE) {
+                    b.append(child.getNodeValue());
                 }
             }
         }
-        for (Node n : toRemove) parent.removeChild(n);
-        for (int i = 0; i < childNodes.getLength(); i++) {
-            final Node child = childNodes.item(i);
-            if (child.getNodeType() == Node.ELEMENT_NODE && !child.getNodeName().equalsIgnoreCase("span") && child.hasChildNodes()) {
-                mergeSpans(child);
+        return b.toString();
+    }
+
+
+    private boolean sameAttrs(NamedNodeMap a1, NamedNodeMap a2) {
+        if (a1.getLength() != a2.getLength()) return false;
+        for (int i=0; i<a1.getLength(); i++) {
+            boolean found = false;
+            final Node a1item = a1.item(i);
+            for (int j=0; j<a2.getLength(); j++) {
+                if (a1item.getNodeName().equalsIgnoreCase(a2.item(j).getNodeName())
+                        && a1item.getNodeValue().equalsIgnoreCase(a2.item(j).getNodeValue())) {
+                    found = true; break;
+                }
             }
+            if (!found) return false;
         }
+        return true;
     }
 }
