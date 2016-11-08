17ba5c530e38745b3d4422af4efa2c53c9c10113
jonathan <jonathan@kyuss.org>
2016-11-08 02:51:38 -0800

    refactor XPath2, add more utility methods

--- src/main/java/org/cobbzilla/util/xml/XPath2.java
+++ src/main/java/org/cobbzilla/util/xml/XPath2.java
@@ -9,15 +9,22 @@ import net.sf.saxon.xpath.XPathFactoryImpl;
 import org.xml.sax.InputSource;
 
 import javax.xml.transform.sax.SAXSource;
-import javax.xml.xpath.*;
+import javax.xml.xpath.XPath;
+import javax.xml.xpath.XPathConstants;
+import javax.xml.xpath.XPathExpression;
+import javax.xml.xpath.XPathFactory;
 import java.io.ByteArrayInputStream;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.concurrent.atomic.AtomicBoolean;
 
 import static org.cobbzilla.util.daemon.ZillaRuntime.die;
 import static org.cobbzilla.util.daemon.ZillaRuntime.empty;
 
 public class XPath2 {
+
+    public static String matchElement (String element) { return "//*[local-name()='"+element+"']"; }
 
     @Getter(lazy=true) private static final XPathFactory xpathFactory = initXpathFactory();
     private static XPathFactory initXpathFactory() {
@@ -28,6 +35,9 @@ public class XPath2 {
             return die("initXpathFactory: "+e, e);
         }
     }
+
+    @Getter(lazy=true) private static final XPath xPath = initXPath();
+    private static XPath initXPath() { return getXpathFactory().newXPath(); }
 
     private static final AtomicBoolean initialized = new AtomicBoolean(false);
     private static void sysinit () {
@@ -40,13 +50,43 @@ public class XPath2 {
         }
     }
 
+    @Getter private Map<String, Path> xpaths = new HashMap<>();
+
+    public XPath2 (String... expressions) {
+        if (empty(expressions)) die("XPath2: no expressions");
+        for (String expr : expressions) {
+            xpaths.put(expr, new Path(expr));
+        }
+    }
+
+    public Map<String, String> firstMatches (String xml) {  return firstMatches(new Doc(xml)); }
+
+    public Map<String, String> firstMatches (Doc doc) {
+        final Map<String, String> matches = new HashMap<>();
+        for (Map.Entry<String, Path> path : xpaths.entrySet()) {
+            final String match = path.getValue().firstMatch(doc);
+            if (!empty(match)) matches.put(path.getKey(), match);
+        }
+        return matches;
+    }
+
+    public String firstMatch(String xml) {
+        switch (xpaths.size()) {
+            case 0:  return die("firstMatch: no xpath expressions");
+            default: return die("firstMatch: more than one xpath expression");
+            case 1:
+                final Map<String, String> matches = firstMatches(xml);
+                return empty(matches) ? null : matches.values().iterator().next();
+        }
+    }
+
     public static class Path {
 
         @Getter private XPathExpression expr;
 
         public Path (String xpath) {
             try {
-                expr = getXpathFactory().newXPath().compile(xpath);
+                expr = getXPath().compile(xpath);
             } catch (Exception e) {
                 die("XPath2.Path: "+e, e);
             }
@@ -80,5 +120,4 @@ public class XPath2 {
             }
         }
     }
-
 }
