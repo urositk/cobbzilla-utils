49451036fc69fa3bd4af51ce7038087e3ebc8074
jonathan <jonathan@kyuss.org>
2016-11-11 12:33:17 -0800

    return closeable http client

--- src/main/java/org/cobbzilla/util/http/PooledHttpClientFactory.java
+++ src/main/java/org/cobbzilla/util/http/PooledHttpClientFactory.java
@@ -3,8 +3,8 @@ package org.cobbzilla.util.http;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import org.apache.http.HttpHost;
-import org.apache.http.client.HttpClient;
 import org.apache.http.conn.routing.HttpRoute;
+import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClients;
 import org.apache.http.impl.conn.PoolingHttpClientConnectionManager;
 import org.cobbzilla.util.reflect.ObjectFactory;
@@ -12,12 +12,12 @@ import org.cobbzilla.util.reflect.ObjectFactory;
 import java.util.Map;
 
 @AllArgsConstructor
-public class PooledHttpClientFactory implements ObjectFactory<HttpClient> {
+public class PooledHttpClientFactory implements ObjectFactory<CloseableHttpClient> {
 
     @Getter private String host;
     @Getter private int maxConnections;
 
-    @Override public HttpClient create() {
+    @Override public CloseableHttpClient create() {
         final PoolingHttpClientConnectionManager cm = new PoolingHttpClientConnectionManager();
         cm.setMaxTotal(maxConnections);
         cm.setMaxPerRoute(new HttpRoute(new HttpHost(host)), maxConnections);
@@ -26,6 +26,6 @@ public class PooledHttpClientFactory implements ObjectFactory<HttpClient> {
                 .build();
     }
 
-    @Override public HttpClient create(Map<String, Object> ctx) { return create(); }
+    @Override public CloseableHttpClient create(Map<String, Object> ctx) { return create(); }
 
 }
