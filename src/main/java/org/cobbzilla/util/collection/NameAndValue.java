4b1ba0ee1eff597cd1728a980b131ff3a2e5326b
jonathan <jonathan@kyuss.org>
2017-01-18 11:53:34 -0800

    add evaluate method

--- src/main/java/org/cobbzilla/util/collection/NameAndValue.java
+++ src/main/java/org/cobbzilla/util/collection/NameAndValue.java
@@ -3,6 +3,10 @@ package org.cobbzilla.util.collection;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.*;
 import lombok.experimental.Accessors;
+import org.cobbzilla.util.javascript.JsEngineDriver;
+import org.cobbzilla.util.javascript.StandardJsEngine;
+
+import java.util.Map;
 
 import static org.cobbzilla.util.daemon.ZillaRuntime.empty;
 
@@ -16,5 +20,26 @@ public class NameAndValue {
     @Getter @Setter private String value;
     public boolean hasValue () { return !empty(value); }
     @JsonIgnore public boolean getHasValue () { return !empty(value); }
+
+    public static NameAndValue[] evaluate (NameAndValue[] pairs, Map<String, Object> context) {
+        return evaluate(pairs, context, StandardJsEngine.DRIVER);
+    }
+
+    public static NameAndValue[] evaluate (NameAndValue[] pairs, Map<String, Object> context, JsEngineDriver engineDriver) {
+
+        if (empty(context) || empty(pairs)) return pairs;
+
+        final NameAndValue[] results = new NameAndValue[pairs.length];
+        for (int i=0; i<pairs.length; i++) {
+            final boolean isCode = pairs[i].getHasValue() && pairs[i].getValue().trim().startsWith("@");
+            if (isCode) {
+                results[i] = new NameAndValue(pairs[i].getName(), engineDriver.evaluate(pairs[i].getValue().trim().substring(1), context, String.class));
+            } else {
+                results[i] = pairs[i];
+            }
+        }
+
+        return results;
+    }
 
 }
