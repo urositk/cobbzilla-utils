2d70bc44a73687162b7ab486008c371339e22bc8
jonathan <jonathan@kyuss.org>
2017-01-20 19:30:18 -0800

    remove failure status

--- src/main/java/org/cobbzilla/util/http/HttpCallStatus.java
+++ src/main/java/org/cobbzilla/util/http/HttpCallStatus.java
@@ -4,7 +4,7 @@ import com.fasterxml.jackson.annotation.JsonCreator;
 
 public enum HttpCallStatus {
 
-    initialized, requested, received_response, success, failure, error, timeout;
+    initialized, requested, received_response, success, error, timeout;
 
     @JsonCreator public static HttpCallStatus fromString (String val) { return valueOf(val.toLowerCase()); }
 
