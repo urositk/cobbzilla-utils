ecc5be2c1ebea87dfbb10593ee04de3983af847b
jonathan <jonathan@kyuss.org>
2017-02-04 02:01:38 -0800

    add ElementIdGenerator

---
+++ src/main/java/org/cobbzilla/util/xml/ElementIdGenerator.java
@@ -0,0 +1,42 @@
+package org.cobbzilla.util.xml;
+
+import lombok.Getter;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+
+import java.util.concurrent.atomic.AtomicInteger;
+
+import static org.cobbzilla.util.daemon.ZillaRuntime.empty;
+
+public class ElementIdGenerator {
+
+    private String prefix = "";
+    private AtomicInteger counter;
+
+    public ElementIdGenerator () { this(1); }
+    public ElementIdGenerator (int start) { counter = new AtomicInteger(start); }
+    public ElementIdGenerator (String prefix) { this(prefix, 1); }
+    public ElementIdGenerator (String prefix, int start) { this.prefix = prefix; counter = new AtomicInteger(start); }
+
+    public Element id(Element e) {
+        if (empty(e.getAttribute("id"))) e.setAttribute("id", prefix+counter.getAndIncrement());
+        return e;
+    }
+
+    public Element create(Document doc, String name) { return id(doc.createElement(name)); }
+
+    public Element text(Document doc, String name, String text) { return text(doc, name, text, null); }
+
+    public Element text(Document doc, String name, String text, Integer truncate) {
+        if (truncate != null && text.trim().length() > truncate) text = text.trim().substring(0, truncate);
+        final Element element = create(doc, name);
+        element.appendChild(doc.createTextNode(text));
+        return element;
+    }
+
+    @Getter(lazy=true) private final XmlElementFunction idFunction = initIdFunction();
+    private XmlElementFunction initIdFunction() {
+        return new XmlElementFunction() { @Override public void apply(Element element) { id(element); } };
+    }
+
+}
