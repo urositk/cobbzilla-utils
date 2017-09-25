ff7a00f8d2e7940142269eeb5cb2b7f6adab0533
Jonathan Cobb <jonathan@kyuss.org>
2017-09-24 18:41:01 -0700

    add HttpContentTypes.TEXT_PLAIN constant

--- src/main/java/org/cobbzilla/util/http/HttpContentTypes.java
+++ src/main/java/org/cobbzilla/util/http/HttpContentTypes.java
@@ -13,6 +13,7 @@ import static org.cobbzilla.util.daemon.ZillaRuntime.empty;
 public class HttpContentTypes {
 
     public static final String TEXT_HTML = "text/html";
+    public static final String TEXT_PLAIN = "text/plain";
     public static final String APPLICATION_JSON = "application/json";
     public static final String APPLICATION_XML = "application/xml";
     public static final String APPLICATION_PDF = "application/pdf";
