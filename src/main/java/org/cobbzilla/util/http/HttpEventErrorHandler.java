a0b080769993204eb8e5a8ada352d8f55c44fe0f
Jonathan Cobb <jonathan@kyuss.org>
2017-11-10 11:24:42 -0800

    move isSuccess to HttpResponseHandler, remove HttpEventErrorHandler

--- src/main/java/org/cobbzilla/util/http/HttpEventErrorHandler.java
+++
@@ -1,8 +0,0 @@
-package org.cobbzilla.util.http;
-
-
-public interface HttpEventErrorHandler {
-
-    boolean isSuccessful(HttpRequestBean request, HttpResponseBean response);
-    void  handleError(HttpRequestBean request, HttpResponseBean response);
-}
--- src/main/java/org/cobbzilla/util/http/HttpResponseHandler.java
+++ src/main/java/org/cobbzilla/util/http/HttpResponseHandler.java
@@ -2,6 +2,7 @@ package org.cobbzilla.util.http;
 
 public interface HttpResponseHandler {
 
+    boolean isSuccess (HttpRequestBean request, HttpResponseBean response);
     void success (HttpRequestBean request, HttpResponseBean response);
     void failure (HttpRequestBean request, HttpResponseBean response);
 
