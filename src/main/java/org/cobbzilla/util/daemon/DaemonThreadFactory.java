dd781e1d6656e0c51b220bcd42397225bf0b1a26
Jonathan Cobb <jonathan@kyuss.org>
2018-02-11 19:14:26 -0800

    ensure valid argument to DaemonThreadFactory.fixedPool

--- src/main/java/org/cobbzilla/util/daemon/DaemonThreadFactory.java
+++ src/main/java/org/cobbzilla/util/daemon/DaemonThreadFactory.java
@@ -1,9 +1,12 @@
 package org.cobbzilla.util.daemon;
 
+import lombok.extern.slf4j.Slf4j;
+
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ThreadFactory;
 
+@Slf4j
 public class DaemonThreadFactory implements ThreadFactory {
 
     public static final DaemonThreadFactory instance = new DaemonThreadFactory();
@@ -14,6 +17,12 @@ public class DaemonThreadFactory implements ThreadFactory {
         return t;
     }
 
-    public static ExecutorService fixedPool (int count) { return Executors.newFixedThreadPool(count, instance); }
+    public static ExecutorService fixedPool (int count) {
+        if (count < 0) {
+            log.warn("fixedPool: invalid count ("+count+"), using single thread");
+            count = 1;
+        }
+        return Executors.newFixedThreadPool(count, instance);
+    }
 
 }
