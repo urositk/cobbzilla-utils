d38084e1494d60c2ef19f03fafb09c4ccc0f1982
Jonathan Cobb <jonathan@kyuss.org>
2018-03-25 14:34:11 -0700

    use regular overrides instead of @Delegate, fixes bug with getMessage

--- src/main/java/org/cobbzilla/util/jdbc/UncheckedSqlException.java
+++ src/main/java/org/cobbzilla/util/jdbc/UncheckedSqlException.java
@@ -1,13 +1,22 @@
 package org.cobbzilla.util.jdbc;
 
 import lombok.AllArgsConstructor;
-import lombok.experimental.Delegate;
 
 import java.sql.SQLException;
 
 @AllArgsConstructor
 public class UncheckedSqlException extends RuntimeException {
 
-    @Delegate private final SQLException sqlException;
+    private final SQLException sqlException;
+
+    @Override public String getMessage() { return sqlException.getMessage(); }
+
+    @Override public String getLocalizedMessage() { return sqlException.getLocalizedMessage(); }
+
+    @Override public synchronized Throwable getCause() { return sqlException.getCause(); }
+
+    @Override public synchronized Throwable initCause(Throwable throwable) { return sqlException.initCause(throwable); }
+
+    @Override public String toString() { return sqlException.toString(); }
 
 }
