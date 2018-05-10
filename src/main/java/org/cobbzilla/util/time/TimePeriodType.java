e321c6cde41dc942c5b58dba8bc9338f9749a33e
Jonathan Cobb <jonathan@kyuss.org>
2018-05-10 08:55:49 -0700

    rename PastTimePeriodType -> TimePeriodType, add more period types

--- src/main/java/org/cobbzilla/util/time/TimePeriodType.java
+++ src/main/java/org/cobbzilla/util/time/TimePeriodType.java
@@ -4,20 +4,22 @@ import com.fasterxml.jackson.annotation.JsonCreator;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 
+import java.util.Arrays;
+
 import static org.cobbzilla.util.daemon.ZillaRuntime.now;
-import static org.cobbzilla.util.time.TimeSpecifier.nowSpecifier;
-import static org.cobbzilla.util.time.TimeSpecifier.todaySpecifier;
+import static org.cobbzilla.util.time.TimeSpecifier.*;
 import static org.cobbzilla.util.time.TimeUtil.*;
 
 @Slf4j @AllArgsConstructor
-public enum PastTimePeriodType {
+public enum TimePeriodType {
 
-    today            (todaySpecifier(),            nowSpecifier()),
+    today            (todaySpecifier(),            tomorrowSpecifier()),
+    tomorrow         (tomorrowSpecifier(),         futureDaySpecifier(2)),
     week_to_date     (t -> startOfWeekMillis(),    nowSpecifier()),
     month_to_date    (t -> startOfMonthMillis(),   nowSpecifier()),
     quarter_to_date  (t -> startOfQuarterMillis(), nowSpecifier()),
     year_to_date     (t -> startOfYearMillis(),    nowSpecifier()),
-    yesterday        (t -> yesterdayMillis(),      todaySpecifier()),
+    yesterday        (yesterdaySpecifier(),        todaySpecifier()),
     previous_week    (t -> lastWeekMillis(),       t -> startOfWeekMillis()),
     previous_month   (t -> lastWeekMillis(),       t -> startOfMonthMillis()),
     previous_quarter (t -> lastQuarterMillis(),    t -> startOfQuarterMillis()),
@@ -26,7 +28,14 @@ public enum PastTimePeriodType {
     private TimeSpecifier startSpecifier;
     private TimeSpecifier endSpecifier;
 
-    @JsonCreator public static PastTimePeriodType fromString (String val) { return valueOf(val.toLowerCase()); }
+    @JsonCreator public static TimePeriodType fromString (String val) {
+        try {
+            return valueOf(val.toLowerCase());
+        } catch (IllegalArgumentException e) {
+            log.warn("fromString("+val+"): invalid value, use one of: "+Arrays.toString(values()));
+            throw e;
+        }
+    }
 
     public long start() { return startSpecifier.get(now()); }
     public long end  () { return endSpecifier.get(now()); }
